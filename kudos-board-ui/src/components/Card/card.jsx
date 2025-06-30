417329eb7554ead4f1e779340c487fba5049a18a
Taliyah Harvey <taliyah.harvey@taliyah-ltm6gk2.internal.salesforce.com>
2025-06-30 16:43:17 -0700

    added card details and updated routes throughout app

---
+++ kudos-board-ui/src/components/Card/card.jsx
@@ -0,0 +1,34 @@
+import defaultImg from "../../assets/smiley.jpg";
+
+import "./card.css";
+
+function card({ card, addUpvote, deleteCard }) {
+  return (
+    <div className="card">
+      <div className="card-info">
+        <div className="info">
+          <span className="message">{card.message}</span>
+        </div>
+        <div className="gif">
+          <Link to={`/${card.id}`}>
+            {card.gif ? (
+              <img src={card.gif} alt="GIF cover" />
+            ) : (
+              <img src={defaultImg} alt="Default GIF cover" />
+            )}
+          </Link>
+        </div>
+        <div className="buttons">
+          <button className="upvoteBtn" onClick={addUpvote}>
+            Upvote: {card.upvotes}
+          </button>
+          <button className="deleteBtn" onClick={deleteCard}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default card;
---
+++ kudos-board-ui/src/pages/boardPage.jsx
@@ -0,0 +1,1 @@
+// insert card grid stuff here

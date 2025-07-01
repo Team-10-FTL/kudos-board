a69d83087958eb65b3dc513833884d9a6e7957a9
Nathan Kenmognie <nkenmognie@nkenmog-ltmr4v9.internal.salesforce.com>
2025-07-01 15:39:07 -0700

    Fixed search bar not filtering working properly

--- kudos-board-ui/src/components/SearchBar/SearchBar.jsx
+++ kudos-board-ui/src/components/SearchBar/SearchBar.jsx
@@ -4,14 +4,14 @@ import { useState, useEffect } from "react";
 
 const VITE_URL = import.meta.env.VITE_URL;
 
-const SearchBar = () => {
+const SearchBar = ({onChange, value}) => {
   const handleClearSearch = () => {
     // to do
   };
 
   return (
     <div>
-      <input name="search-bar" type="text" placeholder="Board Title" />
+      <input name="search-bar" type="text" placeholder="Board Title" value={value} onChange={onChange}/>
       <button className="clearBtn" onClick={handleClearSearch}>
         Clear
       </button>
